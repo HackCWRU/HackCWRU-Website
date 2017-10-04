4602ce68cf068c73cc3b75cbe695e0fc84fc2e79
Jack Mousseau <jackmousseau@gmail.com>
2017-10-04 18:22:18 -0400

    Render the top level App component

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import path    from 'path';
 const PORT = process.env.PORT || 3000;
 
 // This directory will be resolved from inside the lib directory so we need to
-// navigate to the src directory.
-const PUBLIC_DIR = path.resolve(`${__dirname}/../src/public`);
+// navigate to the src directory. Similar requirment for bundles dir.
+const PUBLIC_DIR  = path.resolve(`${__dirname}/../src/public`);
+const BUNDLES_DIR = path.resolve(`${__dirname}/../bundles`);
 
 const app = express();
 
 app.get('/', (req, res, next) => {
   res.sendFile(`${PUBLIC_DIR}/index.html`);
+});
+
+app.get('/index.bundle.js', (req, res, next) => {
+  res.sendFile(`${BUNDLES_DIR}/index.bundle.js`);
 });
 
 app.listen(PORT, () => {
