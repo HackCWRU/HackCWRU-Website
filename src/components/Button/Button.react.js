7d753f434c54d4336d8b20f5120567aaf2c3f0ff
Jack Mousseau <jackmousseau@gmail.com>
2017-10-14 18:26:10 -0400

    Add Button title and externalLink props

--- src/components/Button/Button.react.js
+++ src/components/Button/Button.react.js
@@ -1,15 +1,33 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class Button extends React.Component {
 
-  render() {
+  renderButton() {
     return (
       <div>
-        <form action="http://hack.cwru.edu">
-          <input type="submit" value="Register!" />
-        </form>
+        <button>
+          {this.props.title}
+        </button>
+      </div>
+    )
+  }
+
+  render() {
+    const { externalLink } = this.props;
+
+    return (
+      <div>
+        {externalLink ? <a target="_blank" href={externalLink}>
+          {this.renderButton()}
+        </a> : this.renderButton()}
       </div>
     );
   }
 
 }
+
+Button.propTypes = {
+  title: PropTypes.string,
+  externalLink: PropTypes.string
+};
