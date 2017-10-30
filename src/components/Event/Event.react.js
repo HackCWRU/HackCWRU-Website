b163481b163e8ce14f32be320e90af2155301b22
Michael Smith <mes258@case.edu>
2017-10-30 19:20:35 -0400

    Added Event component

--- src/components/Event/Event.react.js
+++ src/components/Event/Event.react.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import styles from 'components/Event/Event.scss';
 
 export default class Event extends React.Component {
 
   render() {
     return (
       <div>
-        test content
-      </div>
+        <div className={styles.Event}>
+            <h1> The Key Details </h1>
+
+            <p> <span className={styles.details}>Who:</span> From Designers to Programmers, everyone is welcome!  </p>
+            <p> <span className={styles.details}>What:</span> 36 hours of hands on, real world technical creation </p>
+            <p> <span className={styles.details}>When:</span> February 16-18, 2018 </p>
+            <p> <span className={styles.details}>Where:</span> The famous 7 story Sears Think[box] at Case Western Reserve University </p>
+            <p> <span className={styles.details}>Why:</span> To code, build, and learn (Also to get lots and lots of SWAG) </p>
+
+        </div>
+     </div>
     );
   }
 
