1d763293775aaa6b31fa7c4983d324e6553b4dbc
Michael Smith <mes258@case.edu>
2017-11-13 19:41:28 -0500

    Tracks Component

--- src/components/Tracks/Tracks.react.js
+++ src/components/Tracks/Tracks.react.js
@@ -7,8 +7,10 @@ export default class Tracks extends React.Component {
     return (
     	<div className={styles.tracks}>
             <div className={styles.title}>{this.props.title}</div>
-            <div>{this.props.image}</div>
-            <div className={styles.desc}>{this.props.description}</div>
+            <div className={styles.trackRow}>
+                <div>{this.props.image}</div>
+                <div className={styles.desc}>{this.props.description}</div>
+            </div>
     	</div>
     )
   }
