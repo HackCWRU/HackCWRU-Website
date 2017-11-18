663227a4032143c3e2de65d2c40122fb2b21b08d
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 18:59:20 -0500

    Replace Tracks component with Track component

---
+++ src/components/Track/Track.react.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styles from 'components/Track/Track.scss';
+
+export default class Track extends React.Component {
+
+  render() {
+    return (
+    	<div className={styles.track}>
+
+        <div className={styles.nameAndDescription}>
+          <div className={styles.name}>
+            {this.props.name}
+          </div>
+          <div className={styles.description}>
+            {this.props.description}
+          </div>
+        </div>
+    	</div>
+    )
+  }
+
+}
+
+Track.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  description: PropTypes.string.isRequired
+}
