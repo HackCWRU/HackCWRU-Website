c6417150cdfc9b7d53f027ad7b87b11c3b29ebcf
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 16:54:30 -0500

    Remove "The Key Details" from Event component

--- src/components/Event/Event.react.js
+++ src/components/Event/Event.react.js
@@ -7,11 +7,10 @@ export default class Event extends React.Component {
     return (
       <div>
         <div className={styles.Event}>
-            <h1> The Key Details </h1>
-            <div className={styles.title}>{this.props.name}</div>
-            <div className={styles.when}><strong>When: </strong>{this.props.when}</div>
-            <div className={styles.where}><strong>Where: </strong>{this.props.where}</div>
-            <div className={styles.about}><strong>About: </strong>{this.props.about}</div>
+          <div className={styles.title}>{this.props.name}</div>
+          <div className={styles.when}><strong>When: </strong>{this.props.when}</div>
+          <div className={styles.where}><strong>Where: </strong>{this.props.where}</div>
+          <div className={styles.about}><strong>About: </strong>{this.props.about}</div>
         </div>
      </div>
     );
