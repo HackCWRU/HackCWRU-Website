027f706f4228e5c0aba228febf175cc856deaf57
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 17:33:20 -0500

    Improve Event component layout

--- src/components/Event/Event.react.js
+++ src/components/Event/Event.react.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from 'components/Event/Event.scss';
 
 export default class Event extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className={styles.Event}>
-          <div className={styles.title}>{this.props.name}</div>
-          <div className={styles.when}><strong>When: </strong>{this.props.when}</div>
-          <div className={styles.where}><strong>Where: </strong>{this.props.where}</div>
-          <div className={styles.about}><strong>About: </strong>{this.props.about}</div>
+      <div className={styles.event}>
+        <div className={styles.name}>
+          {this.props.name}
         </div>
-     </div>
+        <div className={styles.whenAndWhere}>
+          {`${this.props.when} | ${this.props.where}`}
+        </div>
+        {this.props.description && <div className={styles.description}>
+          {this.props.description}
+        </div>}
+      </div>
     );
   }
 
 }
+
+Event.propTypes = {
+  name: PropTypes.string.isRequired,
+  when: PropTypes.string.isRequired,
+  where: PropTypes.string.isRequired,
+  description: PropTypes.string
+};
