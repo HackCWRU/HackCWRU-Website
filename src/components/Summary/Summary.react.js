ac6b46a879d4f77840de94c6e49fd35c2eb98fb4
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 23:31:25 -0500

    Update summary description

--- src/components/Summary/Summary.react.js
+++ src/components/Summary/Summary.react.js
@@ -9,14 +9,17 @@ export default class Summary extends React.Component {
         <div className={styles.date}>{'February 16th - 18th, 2018'}</div>
         <div className={styles.description}>
           HackCWRU brings together the brightest and most creative students to
-          promote unrestricted technological innovation. Hosted at Case Western
-          Reserve University, over 500 students will spend 36 hours bringing
-          their imagination into the real world. Attendees will work with peers
-          and mentors to create projects in one of six project tracks in the
-          famous 7 story Sears Think[box] makerspace. Stay tuned for more on
-          our track system! Don't be intimidated, however. You don't have to
-          know what you are planning on doing to attend. If you are new to
-          hackathons, we look forward to introducing you to a world of creation.
+          promote unrestricted technological innovation.
+          <div className={styles.spacer}></div>
+          Hosted at Case Western Reserve University, over 350 students will
+          spend 36 hours bringing their imagination into the real world.
+          Attendees will work with peers and mentors to create projects in
+          one of four project tracks.
+          <div className={styles.spacer}></div>
+          Don't be intimidated, however.  You don't have to know what you're
+          doing to attend.  If you are new to hackathons, we look forward to
+          introducing you to a world of creation.
+          Lastly, at HackCWRU we abide by and enforce <a href="http://static.mlh.io/docs/mlh-code-of-conduct.pdf">MLH's Code of Conduct</a>.
         </div>
     	</div>
     )
