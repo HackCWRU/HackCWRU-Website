d7f5a9337dca0b3cbaef68208a5daafebbd9b08d
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 22:52:56 -0500

    Load FAQs and events from Hackry

--- src/web/App.react.js
+++ src/web/App.react.js
@@ -9,8 +9,33 @@ import Event from 'components/Event/Event.react';
 import Track from 'components/Track/Track.react';
 import tracks from 'data/tracks';
 import FAQ from 'components/FAQ/FAQ.react';
+import Hackry from 'hackry';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.hackry = new Hackry('NQZBtoIMDJ');
+    this.state = {
+      faqs: [],
+      events: []
+    };
+  }
+
+  componentWillMount() {
+    this.hackry.faqs((faqs) => {
+      this.setState({
+        faqs: faqs
+      });
+    });
+
+    this.hackry.events((events) => {
+      this.setState({
+        events: events
+      });
+    });
+  }
 
   render() {
     return (
@@ -44,6 +69,28 @@ export default class App extends React.Component {
                  description={track.description} />
               )
             })}
+          </Section>
+          <Section title='FAQs' scrollId='faqs'>
+            {this.state.faqs.length > 0 ? this.state.faqs.map((faq, index) => {
+              return (
+                <FAQ
+                 key={index}
+                 question={faq.question}
+                 answer={faq.answer} />
+              )
+            }) : <div>Coming soon</div>}
+          </Section>
+          <Section title='Schedule' scrollId='schedule'>
+            {this.state.events.length > 0 ? this.state.events.map((event, index) => {
+              return (
+                <Event
+                 key={index}
+                 name={event.name}
+                 when='idk'
+                 where={event.location}
+                 description={event.description} />
+              )
+            }) : <div>Coming soon</div>}
           </Section>
         </div>
       </div>
