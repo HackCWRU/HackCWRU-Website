0b98a465439e186892bddeabcca14b751aac541b
Jack Mousseau <jackmousseau@gmail.com>
2017-11-18 19:13:23 -0500

    Add tracks data

---
+++ src/data/tracks.js
@@ -0,0 +1,19 @@
+module.exports = {
+  all: ['Civic', 'Healthcare', 'Fintech', 'Maker'],
+  civic: {
+    image: '/assets/track_civic.png',
+    description: 'The Civic track looks at the all levels of connected devices, from local IoT networks to municipal systems and civic datasets. Create a new device for your smart home, or create something that benefits the community.'
+  },
+  healthcare: {
+    image: '/assets/track_healthcare.png',
+    description: 'The healthcare track enables hackers to tackle problems relating to medicine, healthcare, and public health. Whether it is using public health data to analyze population wellness or using mobile technology to improve patient quality of and access to care, there has never been a better opportunity to match engineering solutions with real healthcare problems. If your background is in medicine or public health, partner with an engineer and inform the science behind your project!'
+  },
+  fintech: {
+    image: '/assets/track_fin1.png',
+    description: 'The Fintech track is for hackers interested in the market and marketplace. Create a tool to help people make better financial decisions, or make a new algorithm to dominate the stock markets. Or finally figure out what “blockchain” is and what you can do with it. If you have a background in finance, work with an engineer and tackle an interesting problem together!'
+  },
+  maker: {
+    image: '/assets/track_maker.png',
+    description: 'The Maker track enables hackers to unleash their inner maker. Invent something new, or just tinker with a thing to make it do something different. The only requirement is that you Did It Yourself--with your team of 4, of course.'
+  }
+};
